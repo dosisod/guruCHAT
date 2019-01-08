d6bd232d5997430412fb0b3d03ba02ac251d49c3
dosisod <39638017+dosisod@users.noreply.github.com>
2019-01-07 21:44:05 -0800

    initialized code

---
+++ guruchat.js
@@ -0,0 +1,39 @@
+/* characters
+
+\u2500 : ─ : HORX BAR
+\u2502 : │ : VERT BAR
+\u250C : ┌ : UP LEFT CORNER
+\u2510 : ┐ : UP RIGHT CORNER
+\u2514 : └ : LOW LEFT CORNER
+\u2518 : ┘ : LOW RIGHT CORNER
+
+*/
+
+window.onload=()=>{
+	messages=[{"BOB":"HI"},{"user1":"HEY"}]
+	user="BOB" //all non-user usernames will be displayed to the left
+	
+	screen=document.getElementById("x")
+	console.log(screen.innerHTML)
+	screenX=screenY=0
+	
+	while (true) { //finds x width of screen in characters
+		screen.innerHTML+="."
+		if (document.body.clientWidth<screen.scrollWidth) break
+		screenX++
+	}
+	screen.innerHTML=""
+	while (true) {
+		screen.innerHTML+=".\n"
+		if (document.body.clientHeight<screen.scrollHeight) break
+		screenY++
+	}
+	screen.innerHTML=""
+	topbar()
+}
+
+function topbar() {
+	screen.innerHTML+="\n \u250C\u2500\u2500guruCHAT"
+	for (i=0;i<screenX-14;i++) screen.innerHTML+="\u2500"
+	screen.innerHTML+="\u2510"
+}
